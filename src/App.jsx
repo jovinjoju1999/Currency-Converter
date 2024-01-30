2cdf5fcc2c221a1be49749a2b2e06ace3f983443
Jovin Joju <55528193+jovinjoju1999@users.noreply.github.com>
2024-01-30 15:50:57 +0530

    Basic components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button, Grid, TableContainer, Typography } from '@mui/material'
+import { Container } from '@mui/material'
+import InputAmount from './Components/InputAmount'
+import SelectCountry from './Components/SelectCountry'
+import SwitchCurrency from './Components/SwitchCurrency'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const boxStyles = {
+    background: "#fdfdfd",
+    marginTop: "10rem",
+    textAlign: "center",
+    color: "#222",
+    minHeight: "20rem",
+    borderRadius: 2,
+    padding: "4rem 2rem",
+    boxShadow: "0px 10px 15px -3px rgba(0,0,0,0.1)",
+    position: "relative"
+  }
 
   return (
-    <div className="App">
-      
-    </div>
+    <Container maxWidth="md" sx={boxStyles}>
+      <Typography variant="h5" sx={{marginBottom: "2rem"}}>Accurate currency conversions</Typography>
+      <Grid container spacing={2}>
+        <InputAmount/>
+        <SelectCountry/>
+        <SwitchCurrency/>
+        <SelectCountry/>
+      </Grid>
+    </Container>
   )
 }
 
